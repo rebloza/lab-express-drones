0e88b2200f68922080dd5cd7fad008b061930c2d
rebeca <reb19loza@gmail.com>
2022-07-25 17:27:29 +0200

    interacion 1 lista

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,38 @@
 // Iteration #1
+
+// conextion con la DB
+require("../db")
+const mongoose = require("mongoose") 
+
+// elementos a agregar 
+const droneArr = require("./drones.json")
+
+// requerir el modelo
+const Drone = require("../models/Drone.model.js")
+
+//insertar los elemntos
+
+// const addDrones = async() => {
+//     try {
+//         await Drone.insertMany(droneArr)
+//         console.log("Drones agregados")
+//         mongoose.connection.close()
+//     }catch (err) {
+//         next(err)
+//     }
+// }
+// addDrones()
+
+const addDrones = async() => {
+    try {
+        await Drone.create(droneArr)
+        console.log("drones agragados", droneArr) 
+    } catch (err) {
+        next(err)
+    }
+}
+
+addDrones()
+
+
+
